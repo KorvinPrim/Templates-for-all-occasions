8e985c435324ae66c9ad7c19452d66597e5f3dc7
Shashatkin Anton <shashatkin@mail.ru>
2023-07-07 22:29:16 +0400

    The first initializing commit: added a template for GUI applications on the tkinter platform.

---
+++ Templates for tkinter.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+from pathlib import Path
+from tkinter import ttk, Menu
+from tkinter.messagebox import showinfo
+import sys
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # configure the root window
+        self.title("My Awesome App")
+        self.geometry("450x350")
+
+        data_folder = Path("picture/")
+        file_to_open = data_folder / "Avatar_picture.png"
+
+        p1 = tk.PhotoImage(file=file_to_open)
+
+        self.iconphoto(False, p1)
+
+        self.config(menu=MenuBar(self))
+
+        # label
+
+        self.label = ttk.Label(self, text="Hello, Tkinter!")
+        self.label.pack()
+
+        # button
+        self.button = ttk.Button(self, text="Click ME!")
+        self.button['command'] = self.button_clicked
+        self.button.pack()
+
+        # self.config(menu=MenuBar(self)
+
+    def button_clicked(self):
+        showinfo(title="Information",
+                 message="Hello, Tkinter!")
+
+
+class MenuBar(tk.Menu):
+    def __init__(self, parent):
+        tk.Menu.__init__(self, parent)
+
+        filemenu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label="File", underline=0, menu=filemenu)
+        filemenu.add_command(label="New", command=self.callback)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", underline=1, command=self.quit)
+
+        helpmenu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label="Help", menu=helpmenu)
+        helpmenu.add_command(label="About...", command=self.callback)
+
+    def quit(self):
+        sys.exit(0)
+
+    def callback(self):
+        print("called the callback!")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+    exit(0)
