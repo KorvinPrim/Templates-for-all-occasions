f19e7c00684c66e910eb3bf7745ce8783067d2aa
Shashatkin Anton <shashatkin@mail.ru>
2023-08-01 22:40:48 +0400

    Creat temp for fyne url img add for app

---
+++ Golang/FyneTemp/UrlImg/main.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+Url Img
+*/
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/theme"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("H and V Box")
+	w.Resize(fyne.NewSize(500, 500))
+
+	a.Settings().SetTheme(theme.DarkTheme())
+
+	res, _ := fyne.LoadResourceFromURLString("https://phonoteka.org/uploads/posts/2021-07/1625711842_31-phonoteka-org-p-krasivie-arti-kosmosa-krasivo-31.jpg")
+
+	img := canvas.NewImageFromResource(res)
+
+	w.SetContent(img)
+
+	w.ShowAndRun()
+}
+
+//fyne package -os windows -icon myapp.png
